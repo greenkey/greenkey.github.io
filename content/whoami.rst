b62214f51f1ad891221c963b83de8dc2b9399ca7
Lorenzo Mele <greenkey@loman.it>
2017-04-07 23:33:46 +0200

    settings and first page

---
+++ content/whoami.rst
@@ -0,0 +1,17 @@
+Me
+##
+
+:date: 2017-04-07 23:05
+:slug: me
+:lang: en
+:category: top-pages
+:summary: Something about me
+
+> Who is greenkey?
+
+**greenkey** is my nick on Internet, my real name is Lorenzo Mele, I'm from
+Italy and I am a human.
+
+To really know **who am I**, the better way is to chat with me.
+
+If you just want to know **what I do**, you can go to the `What I do <{filename}/whatido.md>`_ page.
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,17 @@ from __future__ import unicode_literals
 
 AUTHOR = 'greenkey'
 SITENAME = 'loman.it'
+SITESUBTITLE = 'home of Lorenzo Mele'
 SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Rome'
 
 DEFAULT_LANG = 'en'
+
+GITHUB_URL = 'https://github.com/greenkey'
+TWITTER_USERNAME = 'greenkey'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -22,14 +26,20 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL_WIDGET_NAME = 'Social'
+SOCIAL = (('Twitter', 'http://twitter.com/' + TWITTER_USERNAME),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Specify name of a built-in theme
+#THEME = "simple"
+
+MENUITEMS = [
+	('Me', 'me')
+]
